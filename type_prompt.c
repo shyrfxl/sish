6ee0d21f86958ae99e764e8a3a971c6599ddfa7b
Will Su <will.suhongyi@gmail.com>
2014-12-16 10:47:25 -0500

    add type_prompt.c for print out the prompt of sish including path, hostname

---
+++ type_prompt.c
@@ -0,0 +1,55 @@
+/*
+ * ============================================================================================
+ * Program Name : type_prompt.c
+ * Author :  Hongyi Su (Will)   hsu4@stevens.edu
+ * Date:     12/14/2014
+ * Description: prepare for prompt of type
+ *
+ *
+ * =============================================================================================
+*/
+#include "sish.h"
+#define Max_name 256
+#define Max_path  1024
+#define Max_all 4096
+
+void type_prompt( char *prompt)
+{
+	extern struct passwd *pwd;
+	char h_name[Max_name] = {"\0"};
+	char p_name[Max_path] = {"\0"};
+	int length = 0;
+	pwd = getpwuid(geteuid());
+	
+	getcwd(p_name, Max_name);
+	if (gethostname(h_name, Max_name) == 0)
+		snprintf(prompt, Max_all, "[sish] %s@%s:",pwd->pw_name, h_name);
+	else
+		snprintf(prompt, Max_all, "[sish] %s@unknown:", pwd->pw_name);
+
+	length = strlen(prompt);
+
+	if(strlen(p_name) < strlen(pwd->pw_dir) || strncmp(p_name, pwd->pw_dir, strlen(pwd->pw_dir)) != 0)
+		snprintf(prompt + length, Max_all, "%s", p_name);
+	else
+		snprintf(prompt + length, Max_all, "~%s", p_name+strlen(pwd->pw_dir));
+	
+	length = strlen(prompt);
+
+	if(geteuid() == 0)
+		snprintf( prompt + length, Max_all,  "#");
+	else
+		snprintf( prompt + length, Max_all,  "$");
+	return;
+}
+
+/*
+int main()
+{
+	char *prompt = NULL;
+	prompt = (char *)malloc( Max_all*sizeof(char));
+	type_prompt( prompt);
+	fprintf( stdout, "%s\n", prompt);
+	return 0;
+}
+*/
