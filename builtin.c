288639dbb6546bb7750fd8147a904fc08cb6dc56
Will Su <will.suhongyi@gmail.com>
2014-12-16 22:21:05 -0500

    add echo built-in

--- builtin.c
+++ builtin.c
@@ -10,7 +10,7 @@
 */
 #include "sish.h"
 
-int builtin_command( char *command, char **parameters, int count)
+int builtin_command( char *command, char **parameters, int count, struct parse_info * info)
 {
 	extern struct passwd *pwd;
 	fprintf(stdout,"builtin:%s\n",command);
@@ -21,8 +21,263 @@ int builtin_command( char *command, char **parameters, int count)
 	}
 	else if( strcmp( command, "echo") == 0)
 	{
-		//
-		fprintf( stdout, "%s\n", parameters[1]);
+		int pipe_fd[2], ret = 0, i = 0, quto = 0;
+		int j = 1, signal = 0;
+		pid_t ChdPid = 0,ChdPid2 = 0;
+		char O_buffer[MAXLINE] = {"\0"}, O_name[Max_name] = {"\0"};
+		char *tmp_Output = NULL ;
+		if((ChdPid = getpid()) < 0)
+		{
+			print_err("Error in getpid()\n");
+		}
+		if(info->flag & IS_PIPED)
+		{
+			i = 1;
+			while(parameters[i] != NULL)
+			{
+				int length = 0;
+				length = strlen(parameters[i]);
+				//fprintf(stdout, "%d\n", length);
+				if (parameters[i][0] == '\"')
+				{
+					quto = 1;
+					j = 1;
+					while(j<length)
+					{
+						if( parameters[i][j++] == '$')
+						{
+							signal = 1;
+							break;
+						}
+						//fprintf( stdout, "22\n");
+					}
+					if (signal == 1)
+					{
+						if(parameters[i][length-1] == '\"')
+							memcpy(O_name, parameters[i]+2, strlen(parameters[i])-3);
+						else
+							memcpy(O_name, parameters[i]+2, strlen(parameters[i])-2);
+						if((tmp_Output = get_env( O_name)) != NULL)
+							strncat(O_buffer,tmp_Output, strlen(tmp_Output));
+						else if(strncmp(O_name,"?",1) == 0)
+						{
+							char Error[10]={"\0"};
+							snprintf(Error,10,"%d",status);
+							strncat(O_buffer, Error,strlen(Error));
+						}
+						else if(strncmp(O_name,"$",1) == 0)
+						{
+							char C_pid[10] = {"\0"};
+							ChdPid = getpid();
+							//fprintf(stdout, "%d\n",ChdPid);
+							snprintf(C_pid, 10, "%d",ChdPid);
+							strncat(O_buffer,C_pid, strlen(C_pid));
+						}
+						//fprintf(stdout, "%s  %s\n", O_name,tmp_Output);
+						memset(O_name, 0, Max_name);
+						strncat(O_buffer," \0", 2);
+						if( parameters[i][length-1] == '\"')
+							quto = 0;
+						i++;
+						signal = 0;
+						continue;		
+					}
+					if( parameters[i][length-1] == '\"')
+					{
+						quto = 0;
+						strncat(O_buffer, parameters[i]+1,length-2);
+					}
+					else
+						strncat(O_buffer, parameters[i]+1,length-1);
+					strncat(O_buffer," \0", 2);	
+					i++;
+					continue;
+				
+				}
+				//fprintf(stdout, "11%s\n",O_buffer);
+				if(parameters[i][0] == '$')
+				{
+					
+					if(parameters[i][length-1] == '\"')	
+						memcpy(O_name, parameters[i]+1, strlen(parameters[i])-1);
+					else
+						memcpy(O_name, parameters[i]+1, strlen(parameters[i]));
+					//fprintf(stdout, "11%s\n", O_name);
+					if((tmp_Output = get_env( O_name)) != NULL)
+						strncat(O_buffer,tmp_Output, strlen(tmp_Output));
+					else if(strncmp(O_name,"?",1) == 0)
+					{
+						char Error[10]={"\0"};
+						snprintf(Error,10,"%d",errno);
+						strncat(O_buffer, Error,strlen(Error));
+					}
+					else if(strncmp(O_name,"$",1) == 0)
+					{
+						char C_pid[10] = {"\0"};
+						ChdPid = getpid();
+						fprintf(stdout, "11 %d\n",ChdPid);
+						snprintf(C_pid, 10, "%d",ChdPid);
+						strncat(O_buffer,C_pid, strlen(C_pid));
+					}
+					memset(O_name, 0, Max_name);
+					strncat(O_buffer," \0", 2);
+					if( parameters[i][length-1] == '\"')
+						quto = 0;
+					i++;
+					continue;												
+				}
+				
+				if( quto == 1 && parameters[i][length-1] == '\"')
+				{
+					strncat(O_buffer, parameters[i],length-1);
+					quto = 0;
+				}
+				else
+					strncat(O_buffer, parameters[i],length);
+				strncat(O_buffer," \0", 2);
+				//fprintf(stdout, "%d:22%s\n",i,O_buffer);
+				i++;
+			}				
+			if(pipe(pipe_fd)<0)
+				print_err("sish error: pipe failed.\n");
+			if((ChdPid2 = fork()) == 0)
+			{
+				
+				close(pipe_fd[1]);
+				close(fileno(stdin));
+				ret = dup2(pipe_fd[0], fileno(stdin));
+				if(ret == -1)
+					print_err("dup2 error: dup2 failed.\n");
+				close(pipe_fd[0]);
+				ret = execvp(info->command2, info->parameters2);
+				if(ret == -1)
+					print_err("execvp error: execvp failed.\n");
+			}
+			else
+			{
+				close(fileno(stdout));
+				ret = dup2(pipe_fd[1], fileno(stdout));
+				fprintf(stdout, "%s", O_buffer);
+				close(pipe_fd[0]);
+				close(pipe_fd[1]);
+				waitpid(ChdPid2, &status, 0);
+			}
+
+			
+		}
+		if(info->flag & BACKGROUND)
+		{
+			fprintf(stdout, "Child pid:%u\n",ChdPid);
+			int i = 0;
+			for( i = 0; i<MAXPIDTABLE;i++)
+				if(BPTable[i] == 0)
+				{
+					BPTable[i] = ChdPid;
+					break;
+				}
+			if( i == MAXPIDTABLE)
+				perror("Too much processes in background\nThere will be zombine process");
+		}
+		else
+		{
+			waitpid( ChdPid, &status, 0);
+		}
+		i = 1;
+		while(i < count)
+		{
+			int length = 0;
+			length = strlen(parameters[i]);
+			//fprintf(stdout, "%d\n", length);
+			if (parameters[i][0] == '\"')
+			{
+				quto = 1;
+				j = 1;
+				while(j<length)
+				{
+					if( parameters[i][j++] == '$')
+					{
+						signal = 1;
+						break;
+					}
+					//fprintf( stdout, "22\n");
+				}
+				if (signal == 1)
+				{
+					if(parameters[i][length-1] == '\"')
+						memcpy(O_name, parameters[i]+2, strlen(parameters[i])-3);
+					else
+						memcpy(O_name, parameters[i]+2, strlen(parameters[i])-2);
+					if((tmp_Output = get_env( O_name)) != NULL)
+						strncat(O_buffer,tmp_Output, strlen(tmp_Output));
+					else if(strncmp(O_name,"?",1) == 0)
+					{
+						char Error[10]={"\0"};
+						snprintf(Error,10,"%d",status);
+						strncat(O_buffer, Error,strlen(Error));
+					}
+					else if(strncmp(O_name,"$",1) == 0)
+					{
+						char C_pid[10] = {"\0"};
+						ChdPid = getpid();
+						snprintf(C_pid, 10, "%d",ChdPid);
+						strncat(O_buffer,C_pid, strlen(C_pid));
+					}
+					//fprintf(stdout, "%s  %s\n", O_name,tmp_Output);
+					memset(O_name, 0, Max_name);
+					strncat(O_buffer," \0", 2);
+					if( parameters[i][length-1] == '\"')
+						quto = 0;
+					i++;
+					continue;		
+				}
+				
+				strncat(O_buffer, parameters[i]+1,length-1);
+				strncat(O_buffer," \0", 2);
+				signal = 0;
+				i++;
+				continue;
+					
+			}
+			if(parameters[i][0] == '$')
+			{
+				
+				if(parameters[i][length-1] == '\"')	
+					memcpy(O_name, parameters[i]+1, strlen(parameters[i])-1);
+				else
+					memcpy(O_name, parameters[i]+1, strlen(parameters[i]));
+				if((tmp_Output = get_env( O_name)) != NULL)
+					strncat(O_buffer,tmp_Output, strlen(tmp_Output));
+				else if(strncmp(O_name,"?",1) == 0)
+				{
+					char Error[10]={"\0"};
+					snprintf(Error,10,"%d",errno);
+					strncat(O_buffer, Error,strlen(Error));
+				}
+				else if(strncmp(O_name,"$",1) == 0)
+				{
+					char C_pid[10] = {"\0"};
+					ChdPid = getpid();
+					snprintf(C_pid, 10, "%d",ChdPid);
+					strncat(O_buffer,C_pid, strlen(C_pid));
+				}
+				memset(O_name, 0, Max_name);
+				strncat(O_buffer," \0", 2);
+				if( parameters[i][length-1] == '\"')
+					quto = 0;
+				i++;
+				continue;												
+			}
+			if( quto == 1 && parameters[i][length-1] == '\"')
+			{
+				strncat(O_buffer, parameters[i],length-1);
+				quto = 0;
+			}
+			else
+				strncat(O_buffer, parameters[i],length);
+			strncat(O_buffer," \0", 2);
+			i++;
+		}
+		fprintf( stdout, "%s\n", O_buffer);
 	}
 	else if( strcmp( command, "cd") == 0)
 	{
