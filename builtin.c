611310b69f97e97e397670d2838c70ebebbcd774
Will Su <will.suhongyi@gmail.com>
2014-12-17 00:28:24 -0500

    echo implement

--- builtin.c
+++ builtin.c
@@ -21,7 +21,7 @@ int builtin_command( char *command, char **parameters, int count, struct parse_i
 	}
 	else if( strcmp( command, "echo") == 0)
 	{
-		int pipe_fd[2], ret = 0, i = 0, quto = 0;
+		int pipe_fd[2], ret = 0,out_fd, i = 0, quto = 0;
 		int j = 1, signal = 0;
 		pid_t ChdPid = 0,ChdPid2 = 0;
 		char O_buffer[MAXLINE] = {"\0"}, O_name[Max_name] = {"\0"};
@@ -155,17 +155,14 @@ int builtin_command( char *command, char **parameters, int count, struct parse_i
 			}
 			else
 			{
-				close(fileno(stdout));
-				ret = dup2(pipe_fd[1], fileno(stdout));
-				fprintf(stdout, "%s", O_buffer);
 				close(pipe_fd[0]);
+				write(pipe_fd[1], O_buffer,strlen(O_buffer));
+				//fprintf(stdout, "%s", O_buffer);
 				close(pipe_fd[1]);
 				waitpid(ChdPid2, &status, 0);
-			}
-
-			
+			}		
 		}
-		if(info->flag & BACKGROUND)
+		else if(info->flag & BACKGROUND)
 		{
 			fprintf(stdout, "Child pid:%u\n",ChdPid);
 			int i = 0;
@@ -180,39 +177,75 @@ int builtin_command( char *command, char **parameters, int count, struct parse_i
 		}
 		else
 		{
-			waitpid( ChdPid, &status, 0);
-		}
-		i = 1;
-		while(i < count)
-		{
-			int length = 0;
-			length = strlen(parameters[i]);
-			//fprintf(stdout, "%d\n", length);
-			if (parameters[i][0] == '\"')
+			i = 1;
+			while(parameters[i]!= NULL)
 			{
-				quto = 1;
-				j = 1;
-				while(j<length)
+				int length = 0;
+				length = strlen(parameters[i]);
+				//fprintf(stdout, "%d\n", length);
+				if (parameters[i][0] == '\"')
 				{
-					if( parameters[i][j++] == '$')
+					quto = 1;
+					j = 1;
+					while(j<length)
 					{
-						signal = 1;
-						break;
+						if( parameters[i][j++] == '$')
+						{
+							signal = 1;
+							break;
+						}
+						//fprintf( stdout, "22\n");
 					}
-					//fprintf( stdout, "22\n");
+					if (signal == 1)
+					{
+						if(parameters[i][length-1] == '\"')
+							memcpy(O_name, parameters[i]+2, strlen(parameters[i])-3);
+						else
+							memcpy(O_name, parameters[i]+2, strlen(parameters[i])-2);
+						if((tmp_Output = get_env( O_name)) != NULL)
+							strncat(O_buffer,tmp_Output, strlen(tmp_Output));
+						else if(strncmp(O_name,"?",1) == 0)
+						{
+							char Error[10]={"\0"};
+							snprintf(Error,10,"%d",status);
+							strncat(O_buffer, Error,strlen(Error));
+						}
+						else if(strncmp(O_name,"$",1) == 0)
+						{
+							char C_pid[10] = {"\0"};
+							ChdPid = getpid();
+							snprintf(C_pid, 10, "%d",ChdPid);
+							strncat(O_buffer,C_pid, strlen(C_pid));
+						}
+						//fprintf(stdout, "%s  %s\n", O_name,tmp_Output);
+						memset(O_name, 0, Max_name);
+						strncat(O_buffer," \0", 2);
+						if( parameters[i][length-1] == '\"')
+							quto = 0;
+						i++;
+						continue;		
+					}
+					
+					strncat(O_buffer, parameters[i]+1,length-1);
+					strncat(O_buffer," \0", 2);
+					signal = 0;
+					i++;
+					continue;
+						
 				}
-				if (signal == 1)
+				if(parameters[i][0] == '$')
 				{
-					if(parameters[i][length-1] == '\"')
-						memcpy(O_name, parameters[i]+2, strlen(parameters[i])-3);
+					
+					if(parameters[i][length-1] == '\"')	
+						memcpy(O_name, parameters[i]+1, strlen(parameters[i])-1);
 					else
-						memcpy(O_name, parameters[i]+2, strlen(parameters[i])-2);
+						memcpy(O_name, parameters[i]+1, strlen(parameters[i]));
 					if((tmp_Output = get_env( O_name)) != NULL)
 						strncat(O_buffer,tmp_Output, strlen(tmp_Output));
 					else if(strncmp(O_name,"?",1) == 0)
 					{
 						char Error[10]={"\0"};
-						snprintf(Error,10,"%d",status);
+						snprintf(Error,10,"%d",errno);
 						strncat(O_buffer, Error,strlen(Error));
 					}
 					else if(strncmp(O_name,"$",1) == 0)
@@ -222,62 +255,44 @@ int builtin_command( char *command, char **parameters, int count, struct parse_i
 						snprintf(C_pid, 10, "%d",ChdPid);
 						strncat(O_buffer,C_pid, strlen(C_pid));
 					}
-					//fprintf(stdout, "%s  %s\n", O_name,tmp_Output);
 					memset(O_name, 0, Max_name);
 					strncat(O_buffer," \0", 2);
 					if( parameters[i][length-1] == '\"')
 						quto = 0;
 					i++;
-					continue;		
+					continue;												
 				}
-				
-				strncat(O_buffer, parameters[i]+1,length-1);
-				strncat(O_buffer," \0", 2);
-				signal = 0;
-				i++;
-				continue;
-					
-			}
-			if(parameters[i][0] == '$')
-			{
-				
-				if(parameters[i][length-1] == '\"')	
-					memcpy(O_name, parameters[i]+1, strlen(parameters[i])-1);
-				else
-					memcpy(O_name, parameters[i]+1, strlen(parameters[i]));
-				if((tmp_Output = get_env( O_name)) != NULL)
-					strncat(O_buffer,tmp_Output, strlen(tmp_Output));
-				else if(strncmp(O_name,"?",1) == 0)
+				if( quto == 1 && parameters[i][length-1] == '\"')
 				{
-					char Error[10]={"\0"};
-					snprintf(Error,10,"%d",errno);
-					strncat(O_buffer, Error,strlen(Error));
-				}
-				else if(strncmp(O_name,"$",1) == 0)
-				{
-					char C_pid[10] = {"\0"};
-					ChdPid = getpid();
-					snprintf(C_pid, 10, "%d",ChdPid);
-					strncat(O_buffer,C_pid, strlen(C_pid));
-				}
-				memset(O_name, 0, Max_name);
-				strncat(O_buffer," \0", 2);
-				if( parameters[i][length-1] == '\"')
+					strncat(O_buffer, parameters[i],length-1);
 					quto = 0;
+				}
+				else
+					strncat(O_buffer, parameters[i],length);
+				strncat(O_buffer," \0", 2);
 				i++;
-				continue;												
+				
 			}
-			if( quto == 1 && parameters[i][length-1] == '\"')
-			{
-				strncat(O_buffer, parameters[i],length-1);
-				quto = 0;
-			}
+			if(!(info->flag & OUT_REDIRECT) && !(info->flag & OUT_REDIRECT_APPEND))
+				fprintf( stdout, "%s\n", O_buffer);
 			else
-				strncat(O_buffer, parameters[i],length);
-			strncat(O_buffer," \0", 2);
-			i++;
+			{
+				if(info->flag & OUT_REDIRECT)
+				{
+					out_fd = open(info->out_file, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+					if(out_fd == -1)
+						print_err("open error: open failed.\n");
+				}
+				else
+				{
+					out_fd = open(info->out_file, O_WRONLY | O_APPEND, 0666);
+					if(out_fd == -1)
+						print_err("open error: open failed.\n");
+				}
+				write(out_fd, O_buffer,strlen(O_buffer));
+				close(out_fd);
+			}
 		}
-		fprintf( stdout, "%s\n", O_buffer);
 	}
 	else if( strcmp( command, "cd") == 0)
 	{
